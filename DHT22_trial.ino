a3f065b1db6d898172190d423aa3b88c4c198739
akkulkarnisj <59623940+akkulkarnisj@users.noreply.github.com>
2020-01-19 17:53:53 -0800

    Works now. Temperature at reset time is used to set thresholds at +1 and +2 above current temperature.

--- DHT22_trial.ino
+++ DHT22_trial.ino
@@ -19,10 +19,13 @@
 // Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
 // Pin 15 can work but DHT must be disconnected during program upload.
 
+#define LEDPIN 2
+#define HEATERPIN 12
+
 // Uncomment the type of sensor in use:
-//#define DHTTYPE    DHT11     // DHT 11
+//efine DHTTYPE    DHT11     // DHT 11
+//efine DHTTYPE    DHT21     // DHT 21 (AM2301)
 #define DHTTYPE    DHT22     // DHT 22 (AM2302)
-//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
 // See guide for details on sensor wiring and usage:
 //   https://learn.adafruit.com/dht/overview
@@ -32,13 +35,12 @@
 DHTesp dht;
 
 uint32_t delayMS;
+float ThresholdHigh = 21;
+float ThresholdLow  = 20;
+int   HeaterPad = LOW;
 
 void setup() {
   Serial.begin(115200);
-
-  // Initialize device.
-  //dht.begin();
-  //Serial.println(F("DHTxx Unified Sensor Example"));
 
   // Initialize temperature sensor
   dht.setup(DHTPIN, DHTesp::DHT22);
@@ -47,87 +49,83 @@ void setup() {
   delayMS=2000;
 
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(2, OUTPUT);
+  pinMode(LEDPIN, OUTPUT);
 
-/*
-  
-  // Print temperature sensor details.
-  sensor_t sensor;
-//  dht.temperature().getSensor(&sensor);
-  Serial.println(F("------------------------------------"));
-  Serial.println(F("Temperature Sensor"));
-  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
-  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
-  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
-  Serial.println(F("------------------------------------"));
-  // Print humidity sensor details.
-//  dht.humidity().getSensor(&sensor);
-  Serial.println(F("Humidity Sensor"));
-  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
-  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
-  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
-  Serial.println(F("------------------------------------"));
-  // Set delay between sensor readings based on sensor details.
-  delayMS = sensor.min_delay / 1000;
-*/
+  // Initialize HeaterPad OFF
+  pinMode(HEATERPIN,OUTPUT);
+  digitalWrite(HEATERPIN,LOW);
+
+  // Get temperature event and print its value.
+  TempAndHumidity newValues = dht.getTempAndHumidity();
+  ThresholdHigh = newValues.temperature + 2;
+  ThresholdLow  = newValues.temperature + 1.9;
 
 }
 
 void loop() {
+
   // Delay between measurements.
   delay(delayMS);
-  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(delayMS);
-  digitalWrite(2, LOW);   // turn the LED on (HIGH is the voltage level)
+  BlinkLedCode(LEDPIN,200,200);
   
-//  // Get temperature event and print its value.
-//  sensors_event_t event;
-//  dht.temperature().getEvent(&event);
-
+  // Get temperature event and print its value.
   TempAndHumidity newValues = dht.getTempAndHumidity();
+
   // Check if any reads failed and exit early (to try again).
   if (dht.getStatus() != 0) {
     Serial.println("DHT11 error status: " + String(dht.getStatusString()));
     return;
   }
-  else {
-    Serial.print(F("Temperature: "));
-    Serial.print(newValues.temperature);
-    Serial.print(F("°C     "));
-    Serial.print((newValues.temperature*9)/5 + 32);
-    Serial.print(F("°F"));
 
-    Serial.print(F("     Humidity: "));
-    Serial.print(newValues.humidity);
-    Serial.println(F("%"));
+  if (newValues.temperature > ThresholdHigh)
+  {
+    // Turn OFF the heater pad
+    HeaterPad = LOW;
   }
+  
+  if (newValues.temperature < ThresholdLow)
+  {
+    // Turn ON the heater pad
+    HeaterPad = HIGH;
+  }
+
+  if (HeaterPad == LOW)
+  {
+    pinMode(HEATERPIN,OUTPUT);  
+    digitalWrite(HEATERPIN,LOW);
+  }
+  else
+  {
+    //digitalWrite(HEATERPIN,HIGH);
+    //delay(10);
+    pinMode(HEATERPIN,INPUT);  
+  }
+
+  //LED
+  digitalWrite(LEDPIN,HeaterPad);
+
+  Serial.print(F("Temperature: "));
+  Serial.print(newValues.temperature);
+  Serial.print(F("°C     "));
+  Serial.print((newValues.temperature*9)/5 + 32);
+  Serial.print(F("°F"));
+
+  Serial.print(F("     Humidity: "));
+  Serial.print(newValues.humidity);
+  Serial.print(F("%"));
+
+  Serial.print(F("     Heater Pad: "));
+  Serial.println(F(HeaterPad == HIGH)?"ON":"OFF");
+
 }
 
-/*
-  if (isnan(event.temperature)) {
-    Serial.println(F("Error reading temperature!"));
-  }
-  else {
-    Serial.print(F("Temperature: "));
-    Serial.print(event.temperature);
-    Serial.println(F("°C"));
-  }
-
-  // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    Serial.println(F("Error reading humidity!"));
-  }
-  else {
-    Serial.print(F("Humidity: "));
-    Serial.print(event.relative_humidity);
-    Serial.println(F("%"));
-  }
+void BlinkLedCode(int NumBlinks, int HighTimeMs, int LowTimeMs)
+{
+    for (int i = 0; i < NumBlinks; i++)
+    {
+        digitalWrite(LEDPIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(HighTimeMs);
+        digitalWrite(LEDPIN, LOW);   // turn the LED on (HIGH is the voltage level)
+        delay(LowTimeMs);
+    }
 }
-*/
